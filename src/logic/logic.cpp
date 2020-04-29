edb878114ca5dcc175d327003d2dfd78c46bbd83
scoutchorton <scoutchorton@yahoo.com>
2020-04-29 12:39:26 -0400

    Moved files to organize in src/ folder

---
+++ src/logic/logic.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "logic.h"
+
+// Not function, takes a string "a" and inverts it and returns the result
+string Not(string a)
+{
+    if (a == preferred) {
+        return unpreferred;
+    } else {
+        return preferred;
+    }
+}
+
+// Basic OR function
+string Or(string a, string b)
+{
+    if ((a == preferred) && (b == preferred)) {
+        return preferred;
+    }
+    if ((a == preferred) || (b == preferred)) {
+        return preferred;
+    }
+    if ((a == unpreferred) && (b == unpreferred)){
+        return unpreferred;
+    }
+}
+
+// Basic AND function
+string And(string a, string b) {
+    if ((a == preferred) && (b == preferred)) {
+        return preferred;
+    }
+    if ((a == unpreferred) || (b == unpreferred)) {
+        return unpreferred;
+    }
+    if ((a == unpreferred) && (b == unpreferred)) {
+        return unpreferred;
+    }
+}
---
+++ src/logic/logic.h
@@ -0,0 +1,6 @@
+string preferred = 1;         //preferred string (defaults to 1)
+string unpreferred = 0;       //unpreferred string (defaults to 0)
+
+string Not(string a);
+string And(string a, string b);
+string Or(string a, string b);
