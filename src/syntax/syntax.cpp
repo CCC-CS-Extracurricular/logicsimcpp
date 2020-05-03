36d585a9f99617c53e33454c45e839ca0c7535c9
unknown <tlee8@corning-cc.edu>
2020-05-02 22:11:58 -0400

    Tim 5/2/2020 Syntax.cpp

--- src/syntax/syntax.cpp
+++ src/syntax/syntax.cpp
@@ -1,1 +1,61 @@
-#include "syntax.h"
+#include <iostream>
+#include <fstream>
+#include "core.h"
+using namespace std;
+global vector<element> Token;
+
+
+int FatRead (string Filepath) {
+//Tim
+//Filepath from core.h
+	string Token;
+	int Location[2];
+	string strParse; //declare vars
+	ifstream ActualFile; //open filepath form core.h
+	ActualFile.open(Filepath);
+	
+	
+			if (ActualFile.is_open()) { //make sure it actually opens the file
+				
+				while (!ActualFile.eof()) {
+					
+					getline(ActualFile, strParse) //reads line by line the file, sends to strParse to hold like a good little guppy
+						if ( strParse.length() == 0 ) { //empty skip
+							continue;
+						};
+						if (strParse[0]= "/"){ //if comment out then skip
+							 continue;
+						};
+						//parse the line by spaces
+							//THING NOTTHING THING NOTTHING
+							Location[0]=strParse[0]
+							
+							Location[1]=strParse.find(" ")
+						
+							Token[0]=strParse.substr(Location[0], Location[1])
+							
+							
+							if ( !(strParse.find(" ",Location[1]))==NULL){
+								Token[1]=
+							};
+							
+							
+							Token[0]=strParse.substr(Location[0], Location[1])
+							
+							
+							//check for declaration of a var
+	
+								if (Token[1] == ArrayNameOfAllOurTypesAndShitThatIDontHaveANameFor[Iter]){
+									element Declaration(Token[0], Token[1]);
+								};
+							
+							}
+				};	
+			};
+	ActualFile.close; 
+ 
+return 0;
+
+ //core.cpp syntax.cpp logic.cpp
+
+}
